24291541b01e851183dcf99591579cc26b11b07c
Austin Bohn <austinbohn@cmu-414574.wv.cc.cmu.edu>
2012-04-16 19:55:45 -0400

    updated model

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ActiveRecord::Base
+  
+  validates_presence_of :content, :name, :email
+  
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([a-z0-9.-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i, :message => "is not a valid format", :allow_blank => true
+  
+  
+end
